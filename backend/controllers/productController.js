18ed30f98a7dc76c9fcdb3c60e7da0d18579a1d7
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-11 18:44:42 +0530

    Get Single Product
    create getSingleProduct handler to get single product data
    add route for getSingleProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,3 +19,20 @@ exports.newProduct = async (req, res, next)=>{
     })
 }
 
+//Get single product - /api/v1/product/:id
+
+exports.getSingleProduct = async(req, res, next) =>{
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
