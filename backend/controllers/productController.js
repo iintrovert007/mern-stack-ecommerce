0fe07c1c711aa445b5cbffa4f222c2282e5922f5
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-11 18:22:35 +0530

    Dispaly All products
    correct each handler with comments
    alter getProducts handler to get multiple products data with count

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,21 @@
 const Product = require('../models/product')
 
-exports.getProducts = (req, res, next)=>{
+//Get all products - /api/v1/products
+exports.getProducts = async (req, res, next)=>{
+    const products = await Product.find();
     res.status(200).json({
         success : true,
-        message : "This route will show all products in database"
+        count: products.length,
+        products
     })
 }
 
+//Create new product 
 exports.newProduct = async (req, res, next)=>{
-    console.log(req.body);
     const product = await  Product.create(req.body);
     res.status(201).json({
         success: true,
         product
     })
 }
+
