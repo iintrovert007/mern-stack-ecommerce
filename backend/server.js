317807f327873120b0c96ab9a9120499872aa016
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-12 19:28:11 +0530

    Handling Unhandled and Uncaught  Errors

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,30 @@ const dotenv = require('dotenv');
 const path = require('path');
 const connectDatabase = require('./config/database');
 
+
+
 dotenv.config({path:path.join(__dirname,'config/config.env')});
 
 connectDatabase();
 
-app.listen(process.env.PORT, ()=>{
+
+
+const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server listening to PORT: ${process.env.PORT} in ${process.env.NODE_ENV} `);
 })
+process.on('uncaughtException',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to uncaughtException exception`)
+    server.close(()=>{
+        process.exit();
+    })
+})
+console.log(a);
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to unhandledRejection exception`)
+    server.close(()=>{
+        process.exit();
+    })
+})
+
