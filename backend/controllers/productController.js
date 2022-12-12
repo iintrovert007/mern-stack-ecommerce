db5ac99894414fed821c6acd9dbe7e1072199407
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-12 19:07:54 +0530

    Catch Async Errors

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,27 +1,28 @@
 const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 //Get all products - /api/v1/products
-exports.getProducts = async (req, res, next)=>{
+exports.getProducts =  async (req, res, next)=>{
     const products = await Product.find();
     res.status(200).json({
         success : true,
         count: products.length,
         products
-    })
+    });
 }
 
 //Create new product 
-exports.newProduct = async (req, res, next)=>{
+exports.newProduct = catchAsyncErrors(async (req, res, next)=>{
     const product = await  Product.create(req.body);
     res.status(201).json({
         success: true,
         product
     })
-}
+});
 
 //Get single product - /api/v1/product/:id
 
-exports.getSingleProduct = async(req, res, next) =>{
+exports.getSingleProduct = catchAsyncErrors(async(req, res, next) =>{
     const product = await Product.findById(req.params.id);
 
     if(!product) {
@@ -32,7 +33,7 @@ exports.getSingleProduct = async(req, res, next) =>{
         success: true,
         product
     })
-}
+});
 
 exports.updateProduct = async(req, res, next)=>{
     let product = await Product.findById(req.params.id);
