4002bdecea0bb39ef42ab1e82898fcc208418e4f
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-13 15:32:02 +0530

    Validation Error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,15 +1,21 @@
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
+   
 
     if( process.env.NODE_ENV === 'production' ){
-       let message = err.message ;
+        let error = {...err};
+
         if(err.name == 'CastError'){
-            message = `Resource not found: ${err.path}`
+            let message = `Resource not found: ${err.path}`
+            error = new Error(message, 400)
+        }
+        if(err.name == 'ValidationError'){
+            let message = Object.values(err.errors).map(value => value.message)
+            error = new Error(message, 400)
         }
         res.status(err.statusCode).json({
             success: false,
-            message: message 
+            message: error.message || 'Internal Server Error' 
         })
     }
     if( process.env.NODE_ENV == 'development' ){
