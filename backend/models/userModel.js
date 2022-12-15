66bba092f71b71a97ccccd3fe89afcbaf5db313d
JVLcode <81806680+jvlcode@users.noreply.github.com>
2022-12-15 11:02:34 +0530

    Generate Forgot password token
    install crypto

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt =  require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
 
 
 const  userSchema = new mongoose.Schema({
@@ -49,6 +50,19 @@ userSchema.methods.getJwtToken = function(){
 
 userSchema.methods.isValidPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.methods.resetPasssword = function() {
+    //Generate token
+    const token = crypto.randomBytes(20).toString('hex');
+
+    //Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex')
+    
+    //Set token expire time
+    this.resetPasswordTokenExpire = Date.now() + 30 * 60 * 1000
+
+    return token;
 }
 
 let schema = mongoose.model('User', userSchema);
