de498885378c271cf8a5b542e2d35d4827f09b6b
JVLcode <81806680+jvlcode@users.noreply.github.com>
2023-01-03 12:13:15 +0530

    Implementing React Router

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,127 +5,127 @@ export default function Home() {
         <div className="container">
             <h1 id="products_heading">Latest Products</h1>
 
-            <section id="products" class="container mt-5">
-            <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div class="card p-3 rounded">
+            <section id="products" className="container mt-5">
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
                     <img
-                    class="card-img-top mx-auto"
+                    className="card-img-top mx-auto"
                     src="./images/products/1.jpg"
                     />
-                    <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">
+                    <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
                         <a href="">OPPO F21s Pro 5G (Dawnlight Gold, 8GB RAM, 128 Storage) with No Cost EMI/Additional Exchange Offers</a>
                     </h5>
-                    <div class="ratings mt-auto">
-                        <div class="rating-outer">
-                        <div class="rating-inner"></div>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                        <div className="rating-inner"></div>
                         </div>
                         <span id="no_of_reviews">(5 Reviews)</span>
                     </div>
-                    <p class="card-text">$245.67</p>
-                    <a href="#" id="view_btn" class="btn btn-block">View Details</a>
+                    <p className="card-text">$245.67</p>
+                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                     </div>
                 </div>
                 </div>
         
-                <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div class="card p-3 rounded">
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
                     <img
-                    class="card-img-top mx-auto"
+                    className="card-img-top mx-auto"
                     src="./images/products/2.jpg"
                     />
-                    <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">
+                    <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
                         <a href=""
                         >WRISTIO HD, Bluetooth Calling Smart Watch, 15 days battery life, Water Resistant</a
                         >
                     </h5>
-                    <div class="ratings mt-auto">
-                    <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-half-o"></i>
-                        <i class="fa fa-star-o"></i>
+                    <div className="ratings mt-auto">
+                    <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star-half-o"></i>
+                        <i className="fa fa-star-o"></i>
                         <span id="no_of_reviews">(5 Reviews)</span>
                     </div>
-                    <p class="card-text">$150.32</p>
-                    <a href="#" id="view_btn" class="btn btn-block">View Details</a>
+                    <p className="card-text">$150.32</p>
+                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                     </div>
                 </div>
                 </div>
         
-                <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div class="card p-3 rounded">
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
                     <img
-                    class="card-img-top mx-auto"
+                    className="card-img-top mx-auto"
                     src="./images/products/3.jpg"
                     />
-                    <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">
+                    <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
                         <a href=""
                         >Dell Inspiron 3511 Laptop, Intel i3-1115G4, 8GB, 512GB</a
                         >
                     </h5>
-                    <div class="ratings mt-auto">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-half-o"></i>
-                        <i class="fa fa-star-o"></i>
+                    <div className="ratings mt-auto">
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star-half-o"></i>
+                        <i className="fa fa-star-o"></i>
                         <span id="no_of_reviews">(5 Reviews)</span>
                     </div>
-                    <p class="card-text">$440.57</p>
-                    <a href="#" id="view_btn" class="btn btn-block">View Details</a>
+                    <p className="card-text">$440.57</p>
+                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                     </div>
                 </div>
                 </div>
         
-                <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div class="card p-3 rounded">
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
                     <img
-                    class="card-img-top mx-auto"
+                    className="card-img-top mx-auto"
                     src="./images/products/4.jpg"
                     />
-                    <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">
+                    <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
                         <a href="">PTron Newly Launched Tangent Sports, 60Hrs Playtime</a>
                     </h5>
-                    <div class="ratings mt-auto">
-                    <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-half-o"></i>
-                        <i class="fa fa-star-o"></i>
+                    <div className="ratings mt-auto">
+                    <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star-half-o"></i>
+                        <i className="fa fa-star-o"></i>
                         <span id="no_of_reviews">(5 Reviews)</span>
                     </div>
-                    <p class="card-text">$15.46</p>
+                    <p className="card-text">$15.46</p>
         
-                    <a type="button" href="#" id="view_btn" class="btn btn-block">View Details</a>
+                    <a type="button" href="#" id="view_btn" className="btn btn-block">View Details</a>
                     </div>
                 </div>
                 </div>
         
-                <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-                <div class="card p-3 rounded">
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
                     <img
-                    class="card-img-top mx-auto"
+                    className="card-img-top mx-auto"
                     src="./images/products/5.jpg"
                     />
-                    <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">
+                    <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
                         <a href="">Campus Men's Maxico Running Shoes</a>
                     </h5>
-                    <div class="ratings mt-auto">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-half-o"></i>
-                        <i class="fa fa-star-o"></i>
+                    <div className="ratings mt-auto">
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star-half-o"></i>
+                        <i className="fa fa-star-o"></i>
                         <span id="no_of_reviews">(5 Reviews)</span>
                     </div>
-                    <p class="card-text">$10.12</p>
-                    <a href="#" id="view_btn" class="btn btn-block">View Details</a>
+                    <p className="card-text">$10.12</p>
+                    <a href="#" id="view_btn" className="btn btn-block">View Details</a>
                     </div>
                 </div>
                 </div>
--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -3,8 +3,8 @@ import { Fragment } from "react";
 export default  function Footer () {
     return (
         <Fragment>
-            <footer class="py-1">
-                <p class="text-center mt-1">
+            <footer className="py-1">
+                <p className="text-center mt-1">
                     JVLcart - 2022-2023, All Rights Reserved
                 </p>
             </footer>
