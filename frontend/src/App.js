17618d786ba55750fd7871bfe6033d98f3e769ea
JVLcode <81806680+jvlcode@users.noreply.github.com>
2023-01-03 12:27:06 +0530

    Adding Title using Helmet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,19 @@ import Home from './components/Home';
 import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <HelmetProvider>
           <Header/>
           <Routes>
             <Route path='/' element={<Home/>} />
           </Routes>
           <Footer/>
+        </HelmetProvider>
       </div>
     </Router>
   );
