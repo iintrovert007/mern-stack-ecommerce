122682435b21db72c8cc0692a0f59edf7acd01b0
JVLcode <81806680+jvlcode@users.noreply.github.com>
2023-01-22 13:24:18 +0530

    Show user in header

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
 import Search from './Search';
+import { DropdownButton, Dropdown, Image } from 'react-bootstrap';
 
 export default function Header () {
+
+    const { user } = useSelector(state => state.authState);
+
     return (
     <nav className="navbar row">
         <div className="col-12 col-md-3">
@@ -18,8 +23,23 @@ export default function Header () {
         </div>
   
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          { user ? (
+            <Dropdown className='d-inline' variant="">
+            <Dropdown.Toggle variant="default text-white pr-5" id="dropdown-basic">
+              <figure className='avatar avatar-nav'>
+              <Image width="50px" src="./images/default_avatar.png" fluid   />
+              </figure>
+              <span>{user.name}</span>
+            </Dropdown.Toggle>
+          
+            <Dropdown.Menu>
+              <Dropdown.Item className='text-danger' >Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          ) 
+          :
           <Link className="btn" to={"/login"} id="login_btn">Login</Link>
-  
+          }
           <span id="cart" className="ml-3">Cart</span>
           <span className="ml-1" id="cart_count">2</span>
         </div>
