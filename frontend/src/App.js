18491695ad4087e6d424b724e86ab4e36c19ab9f
JVLcode <81806680+jvlcode@users.noreply.github.com>
2023-02-04 16:05:16 +0530

    Cart Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import UpdateProfile from './components/user/UpdateProfile';
 import UpdatePassword from './components/user/UpdatePassword';
 import ForgotPassword from './components/user/ForgotPassword';
 import ResetPassword from './components/user/ResetPassword';
+import Cart from './components/cart/Cart';
 
 function App() {
 
@@ -44,6 +45,7 @@ function App() {
                       <Route path='/myprofile/update/password' element={<ProtectedRoute><UpdatePassword/></ProtectedRoute> } />
                       <Route path='/password/forgot' element={<ForgotPassword/> } />
                       <Route path='/password/reset/:token' element={<ResetPassword/> } />
+                      <Route path='/cart' element={<Cart/> } />
                   </Routes>
                 </div>
             <Footer/>
