f0a0fe4ddc770e56fe952468d329f5551c43ee3a
JVLcode <81806680+jvlcode@users.noreply.github.com>
2023-02-05 09:59:40 +0530

    Confirm Order details

--- frontend/src/components/cart/CheckoutSteps.js
+++ frontend/src/components/cart/CheckoutSteps.js
@@ -5,12 +5,12 @@ export default function CheckoutSteps({shipping, confirmOrder, payment}) {
         <div className="checkout-progress d-flex justify-content-center mt-5">
 
             {shipping ? 
-            <Link to="shipping">
+            <Link to="/shipping">
                 <div className="triangle2-active"></div>
                 <div className="step active-step">Shipping Info</div>
                 <div className="triangle-active"></div>
             </Link>
-            :<Link to="shipping">
+            :<Link to="/shipping">
             <div className="triangle2-incomplete"></div>
             <div className="step incomplete">Shipping Info</div>
             <div className="triangle-incomplete"></div>
